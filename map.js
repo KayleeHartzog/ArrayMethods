939ce67a2e05c7442f2b4bacd42edd044da014fa
KayleeHartzog <har15031@byui.edu>
2019-05-01 14:58:26 -0600

    started map.js

---
+++ map.js
@@ -0,0 +1,14 @@
+//1. What does it do? make sure to explain all the parameters. If it has a function as a parameter, make sure to explain all of the parameters for that function.
+// the map function takes the elements in the array and makes a change to each item, changes are specified by a function. In the parameters, you put the function
+// you want to use.
+
+//2. Does it edit the current array?
+// Yes, it changes each element in the array, according to the specifications made in the function.
+
+//3. What does it return?
+//It returns an array of the outputs of the elements.
+
+//4. How can I use this? Come up (not one off the internet) with a small real world example and explain it.
+
+
+//5. Build your real world example.
